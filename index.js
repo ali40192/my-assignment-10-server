f6020f6f640109b703c20074caa4220bfba63751
Mohammad Ali <mohammadali40192@gmail.com>
2026-01-04 19:30:18 +0600

    added a sec

--- index.js
+++ index.js
@@ -11,6 +11,38 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.fyk0nds.mongodb.net/?appName=Cluster0`;
 
+// const verifyJWT = async (req, res, next) => {
+//   const token = req?.headers?.authorization?.split(" ")[1];
+//   // console.log(token);
+//   if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+//   try {
+//     const decoded = await admin.auth().verifyIdToken(token);
+//     req.tokenEmail = decoded.email;
+//     // console.log(decoded);
+//     next();
+//   } catch (err) {
+//     // console.log(err);
+//     return res.status(401).send({ message: "Unauthorized Access!", err });
+//   }
+// };
+
+//or
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  req.tokenEmail = req.headers.email;
+  next();
+};
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,6 +58,7 @@ async function run() {
     const db = client.db("my-bookCollection");
     const booksCollection = db.collection("books");
     const loggedusersCollection = db.collection("comment-collection");
+    const usersCollection = db.collection("users");
 
     ////1.Get all
     app.get("/allbooks", async (req, res) => {
@@ -100,6 +133,61 @@ async function run() {
         .find({ userEmail: email })
         .toArray();
       res.send(result);
+    });
+
+    ////user collection create or update //// all kinds of user here
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      user.created_at = new Date().toISOString();
+      user.last_loggedIn = new Date().toISOString();
+      user.role = "user";
+      user.status = "active";
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        const result = await usersCollection.updateOne(query, {
+          $set: { last_loggedIn: new Date().toISOString() },
+        });
+        return res.send(result);
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    /////get user role   ///
+    // app.get("/users/role", verifyJWT, async (req, res) => {
+    //   console.log(req.tokenEmail);
+
+    //   const result = await usersCollection.findOne({ email: req.tokenEmail });
+    //   res.send({ role: result?.role });
+    // });
+
+    // or
+    app.get("/users/role", verifyJWT, async (req, res) => {
+      const email = req.tokenEmail;
+
+      if (!email) {
+        return res.status(400).send({ message: "Email missing" });
+      }
+
+      const user = await usersCollection.findOne({ email });
+
+      if (!user) {
+        return res.send({ role: "guest" });
+      }
+
+      res.send({ role: user.role });
+    });
+
+    /////get profile data
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const userDetails = await usersCollection.findOne(query);
+      res.send(userDetails);
     });
 
     // Send a ping to confirm a successful connection
